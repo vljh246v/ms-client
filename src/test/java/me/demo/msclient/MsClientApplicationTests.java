0afcd6af3bc8ac49d2a71eaec562a177e5f093c2
jaehyun <vljh246v@naver.com>
2020-11-24 01:58:36 +0900

    Initial commit

---
+++ src/test/java/me/demo/msclient/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.demo.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
